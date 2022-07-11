bc33de5e60f6b0b3895ba494e9b8e4051e39c913
orlov332 <orlov332@gmail.com>
2022-07-12 00:07:01 +0200

    conf: OpenApi documentation server config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import * as YAML from 'yamljs';
+import { join } from 'path';
+
+const PORT = 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  const document = YAML.load(join(__dirname, '../doc/api.yaml'));
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(PORT).then(() => console.log('Server started at port🚀: ', PORT));
 }
+
 bootstrap();
