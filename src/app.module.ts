790d69b80bbcee9c3bd5f1a192230ed42f7ccacf
orlov332 <orlov332@gmail.com>
2022-07-14 08:47:16 +0200

    feat: Artist resource template

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { ArtistModule } from './artist/artist.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule, ArtistModule],
 })
 export class AppModule {}
