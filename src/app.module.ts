64b57c661496b8a096c2b755a99410facaa740f0
orlov332 <orlov332@gmail.com>
2022-07-14 10:31:33 +0200

    feat: Album resource template

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { ArtistModule } from './artist/artist.module';
+import { AlbumModule } from './album/album.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule, ArtistModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule, ArtistModule, AlbumModule],
 })
 export class AppModule {}
