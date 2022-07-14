25b9bb5c98ef77c65238935e10066880a5e9f86e
orlov332 <orlov332@gmail.com>
2022-07-14 09:54:44 +0200

    refactor: Improve User service encapsulation

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,21 +1,19 @@
 import {
-  Controller,
-  Get,
-  Post,
   Body,
-  Param,
-  Delete,
-  ParseUUIDPipe,
-  NotFoundException,
-  HttpCode,
-  UseInterceptors,
   ClassSerializerInterceptor,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  ParseUUIDPipe,
+  Post,
   Put,
+  UseInterceptors,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { User } from './entities/user.entity';
 
 @Controller('user')
 export class UserController {
@@ -24,35 +22,30 @@ export class UserController {
   @Post()
   @UseInterceptors(ClassSerializerInterceptor)
   create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto).then((user) => new User(user));
+    return this.userService.create(createUserDto);
   }
 
   @Get()
   @UseInterceptors(ClassSerializerInterceptor)
   async findAll() {
-    const users = await this.userService.findAll();
-    return users.map((user) => new User(user));
+    return await this.userService.findAll();
   }
 
   @Get(':id')
   @UseInterceptors(ClassSerializerInterceptor)
   async findOne(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
-    const user = await this.userService.findOne(id);
-    if (user) return new User(user);
-    else throw new NotFoundException();
+    return this.userService.findOne(id);
   }
 
   @Put(':id')
   @UseInterceptors(ClassSerializerInterceptor)
   update(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(id, updateUserDto).then((user) => new User(user));
+    return this.userService.update(id, updateUserDto);
   }
 
   @Delete(':id')
   @HttpCode(204)
   async remove(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
-    const deleted = await this.userService.remove(id);
-    if (deleted) return deleted;
-    else throw new NotFoundException();
+    return this.userService.remove(id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,10 +2,11 @@ import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/commo
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserRepository } from './user.repository';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(private readonly repository: UserRepository) {}
 
   create(createUserDto: CreateUserDto) {
     const user = {
@@ -14,19 +15,21 @@ export class UserService {
       createdAt: Date.now(),
       updatedAt: Date.now(),
     };
-    return this.userRepository.create(user);
+    return this.repository.create(user).then((user) => new User(user));
   }
 
   findAll() {
-    return this.userRepository.findAll();
+    return this.repository.findAll().then((users) => users.map((user) => new User(user)));
   }
 
-  findOne(id: string) {
-    return this.userRepository.findOne(id);
+  async findOne(id: string) {
+    const user = await this.repository.findOne(id);
+    if (user) return new User(user);
+    else throw new NotFoundException();
   }
 
   async update(id: string, updateUserDto: UpdateUserDto) {
-    const user = await this.userRepository.findOne(id);
+    const user = await this.repository.findOne(id);
     if (user) {
       if (user.password === updateUserDto.oldPassword) {
         const updateObj = {
@@ -35,12 +38,14 @@ export class UserService {
           version: user.version + 1,
           updatedAt: Date.now(),
         };
-        return await this.userRepository.update(id, updateObj);
+        return await this.repository.update(id, updateObj).then((user) => new User(user));
       } else throw new ForbiddenException();
     } else throw new NotFoundException();
   }
 
-  remove(id: string) {
-    return this.userRepository.remove(id);
+  async remove(id: string) {
+    const deleted = await this.repository.remove(id);
+    if (deleted) return deleted;
+    else throw new NotFoundException();
   }
 }
